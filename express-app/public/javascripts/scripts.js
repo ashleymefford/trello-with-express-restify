a98a186a83988d393bc4e99a4281931ceed79232
Shalay Mefford <shalay.mefford@inventive.io>
2018-02-28 16:20:29 -0600

    css and jquery are working

--- express-app/public/javascripts/scripts.js
+++ express-app/public/javascripts/scripts.js
@@ -1,19 +1,59 @@
 $(document).ready(function() {
-	$("#btnAddMessage").on("click", function() {
+    $('.addSwimLane').on("click", function() {
+        let swimlane =
+            '<div class="swimlane">' +
+            '<div class="swimlaneTitle">Add Swimlane Title</div>' +
+            '<button class="addCard">Add Card</button>' +
+            '<button class="removeSwimlane">Remove</button>' +
+            '</div>';
+        $('.container').append(swimlane);
+    });
 
-		var message = {
-			id: $("#txtId").val(),
-			subject: $("#txtSubject").val(),
-			description: $("#txtDescription").val()
-		}
+    $('.container').on('click', '.swimlaneTitle', function(){
+        let swimlaneTitle= prompt('What is the Swimlane title?');
+        $(this).closest('.swimlaneTitle').text(swimlaneTitle);
+    });
 
-		$.ajax({
-				method: "POST",
-				url: "http://localhost:8080/messages",
-				data: message
-			})
-			.done(function(msg) {
-				alert("Data Saved: " + msg);
-			});
-	});
+    $('.container').on('click', '.removeSwimlane', function() {
+        $(this).closest('.swimlane').remove();
+    });
+
+    $('.container').on('click', '.addCard', function() {
+        let taskCard =
+            '<div class="taskCard">' +
+            '<button class="removeCard">X</button>' +
+            '<div class="cardTitle">Add Card Title</div>' +
+            '<div class="cardDescription">Add Card Description</div>' +
+            '</div>';
+        $(this).closest('.swimlane').append(taskCard);
+    });
+
+    $('.container').on('click', '.cardTitle', function(){
+        let cardTitle= prompt('What is the card title?');
+        $(this).closest('.cardTitle').text(cardTitle);
+    });
+
+    $('.container').on('click', '.cardDescription', function(){
+        let cardDescription= prompt('What is the card description?');
+        $(this).closest('.cardDescription').text(cardDescription);
+    });
+
+    $('.container').on('click', '.removeCard', function() {
+        $(this).closest('.taskCard').remove();
+    });
+
+    var message = {
+        id: $("#txtId").val(),
+        subject: $("#txtSubject").val(),
+        description: $("#txtDescription").val()
+    }
+
+    $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/messages",
+            data: message
+        })
+        .done(function(msg) {
+            alert("Data Saved: " + msg);
+        });
 });
